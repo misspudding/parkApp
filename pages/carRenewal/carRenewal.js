e01b32fe1a266d5405324e17649b2db6b0572245
cherry <472979229@qq.com>
2018-07-18 15:37:16 +0800

    v1.1添加了picker功能

--- pages/carRenewal/carRenewal.js
+++ pages/carRenewal/carRenewal.js
@@ -1,66 +1,36 @@
 // pages/carRenewal/carRenewal.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    array: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+    index: 0,
+    money:300,
+    date:'2018.01.01',
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  bindPickerChange: function (e) {
+    var that = this;
+    var addMonth = this.data.array[e.detail.value];
+    var date = that.dateChange(addMonth);
+    this.setData({
+      index: e.detail.value,
+      date:date
+    })
+    
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  dateChange: function (e) {
+    //console.log("inner dateChange")
+    //console.log(e);
+    var addMonth = e;
+    var timeArray = this.data.date.split('.');
+    var year = parseInt(timeArray[0]);
+    var month = parseInt(timeArray[1]) + parseInt(addMonth);
+    var day = parseInt(timeArray[2]);
+    if (month > 12) {
+      month = 1;
+      year += 1;
+    }
+    var date = (year + '.' + month + '.' + day)
+    return date;
   }
 })
