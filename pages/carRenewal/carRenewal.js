57514fcc4cc4ced8052b4615d86f2c38b3e6ce4b
cherry <472979229@qq.com>
2018-07-19 09:58:01 +0800

    v1.1添加了picker功能

--- pages/carRenewal/carRenewal.js
+++ pages/carRenewal/carRenewal.js
@@ -6,28 +6,32 @@ Page({
     index: 0,
     money:300,
     date:'2018.01.01',
+    tempDate:'2018.01.01'
   },
 
   bindPickerChange: function (e) {
     var that = this;
-    var addMonth = this.data.array[e.detail.value];
-    var date = that.dateChange(addMonth);
+    var tempDate = that.dateChange(e);
     this.setData({
       index: e.detail.value,
-      date:date
+      tempDate: tempDate
     })
     
   },
   dateChange: function (e) {
-    //console.log("inner dateChange")
-    //console.log(e);
-    var addMonth = e;
     var timeArray = this.data.date.split('.');
     var year = parseInt(timeArray[0]);
+    var addMonth = this.data.array[e.detail.value];
     var month = parseInt(timeArray[1]) + parseInt(addMonth);
+    console.log("originalMonth:" + parseInt(timeArray[1]));
     var day = parseInt(timeArray[2]);
     if (month > 12) {
-      month = 1;
+      month ='0'+ 1;
+      if(month < 10){
+        month = '0' + month
+      }else{
+        
+      }
       year += 1;
     }
     var date = (year + '.' + month + '.' + day)
